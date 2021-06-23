313ce89e643ec77fd8cc2c4ae3a9ebc09aa9b70b
hijuliancode <hijuliansosa@gmail.com>
2021-06-23 11:19:37 -0500

    torre.co | Services: search users & jobs

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,68 @@
+import axios from 'axios';
+import { useContext } from 'vue';
 import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    loading: false,
+    jobs_results: [],
+    users_results: [],
+    job_selected: {},
+    user_selected: {},
   },
   mutations: {
+    setJobs(state, payload) {
+      state.jobs_results = payload;
+    },
+    setUsers(state, payload) {
+      state.users_results = payload;
+    },
+    setJobSelected(state, payload) {
+      state.job_selected = payload;
+    },
+    setUserSelected(state, payload) {
+      state.user_selected = payload;
+    },
+    loading(state, payload) {
+      state.loading = payload;
+    },
   },
   actions: {
+    async searchJobs(context, query) {
+      // context.commit('loading', true);
+      try {
+        const { data } = await axios.post('https://search.torre.co/opportunities/_search/?offset=0&size=4&aggregate=true', {
+          body: {
+            'skill/role': {
+              text: query,
+              experience: 'potential-to-develop',
+            },
+          },
+        });
+        context.commit('setJobs', data);
+        context.commit('loading', false);
+        console.log('searchJobs data=>', data);
+      } catch (err) {
+        console.warn('searchJobs err => ', err);
+      }
+    },
+    async searchUsers(context, query) {
+      // context.commit('loading', true);
+      try {
+        const { data } = await axios.post('https://search.torre.co/people/_search/?offset=0&size=4&aggregate=true', {
+          body: {
+            name: {
+              term: query,
+            },
+          },
+        });
+        context.commit('setUsers', data);
+        context.commit('loading', false);
+        console.log('searchUsers data=>', data);
+      } catch (err) {
+        console.warn('searchUsesr err => ', err);
+      }
+    },
   },
   modules: {
   },
--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -2,29 +2,47 @@
   <div class="page-search-results">
     SearchResults
 
-    <button @click="handleClick" >Click</button>
+    <form @submit.prevent="searchJobs(search.query)">
+      <input type="text" placeholder="Buscar trabajo..." v-model="search.queryJobs"/>
+      <button type="submit">Buscar Trabajo</button>
+    </form>
+    <form @submit.prevent="searchUsers(search.query)">
+      <input type="text" placeholder="Buscar usuarios..." v-model="search.queryUsers"/>
+      <button type="submit">Buscar Usuarios</button>
+    </form>
+    {{search}}
+
+    <div class="loading" v-if="loading">loading...</div>
+
+    <hr/>
+    {{jobs_results}}
+    {{users_results}}
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { mapActions, mapState } from 'vuex';
 // import HeaderNav from '@/components/HeaderNav.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'SearchResults',
+  computed: {
+    ...mapState(['loading', 'jobs_results', 'users_results']),
+  },
   data() {
     return {
-      results: [],
+      search: {
+        queryJobs: '',
+        queryUsers: '',
+      },
     };
   },
   methods: {
-    handleClick() {
-      console.log('Haz hecho click', this.results);
-    },
+    ...mapActions(['searchJobs', 'searchUsers']),
   },
   async mounted() {
-    const { data } = await this.axios.get('https://jsonplaceholder.typicode.com/users');
-    this.results = data;
+    console.log('mounted');
   },
 });
 </script>
