1b895f2a7ca53b692912771d42d765ceb780680a
hijuliancode <hijuliansosa@gmail.com>
2021-06-22 19:59:05 -0500

    torre.co | Remove unnecesary code | Add HeaderNav markup

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <nav id="nav" class="header__nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/users">Users</router-link>
+      <router-link to="/jobs">Jobs</router-link>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HeaderNav',
+});
+</script>
